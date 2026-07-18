1a41d53f68c514ed7cceb86bcc37c67bd30a4765
ManddarinShop <dudgh102416@gmail.com>
2026-07-19 01:37:03 +0900

    chore: remove unused core and gateway code, update gitignore
    - Remove src/core/ (queued/direct repository, schema, shared types)
    - Remove templates/manual-apps-script-gateway/
    - Add .local/, review/, design/ to .gitignore

--- src/core/schema/index.ts
+++
@@ -1,6 +0,0 @@
-export { boolean, number, text } from "./Columns.js";
-export { parseRow } from "./RowParser.js";
-export { assertSchema } from "./SheetSchema.js";
-export type { Column } from "./Columns.js";
-export type { ParseRowInput } from "./RowParser.js";
-export type { SchemaColumnMap } from "./SheetSchema.js";
